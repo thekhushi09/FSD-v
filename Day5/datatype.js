46d0a163db9455b07f07fab9dd64d4d8dd520db0
thekhushi09 <thekhushitomar10@gmail.com>
2024-10-14 10:23:06 +0530

    new

--- Day5/datatype.js
+++ Day5/datatype.js
@@ -37,3 +37,54 @@ let arr=[12,13,14,15,16]
 let a;
 // console.log(typeof a)                // undefined
 
+
+let z=Number('13');
+let s=Number('14');
+// console.log(z+s);
+// console.log(typeof z);
+
+let b=Boolean(0);                      //false
+// console.log(b);      
+
+"use strict"
+let num='12';
+// console.log(num)
+
+// strict equality operator   (===)
+let c=12;
+let d="12";
+
+// console.log(c==d);
+// console.log(c===d);
+
+
+// ARROW FUNCTION
+
+let ar  = () => {
+    console.log("hello")
+
+};
+ar();
+
+let ar1  = (x,y) => {
+    console.log(x+y);
+
+};
+ar1(10,5);
+
+
+let arr1  = (x,y) => {
+    return(x+y);
+
+};
+let res=arr1(10,5);
+console.log(res);
+
+
+let sayHi = () => console.log("hello");
+sayHi();
+
+
+
+
+
