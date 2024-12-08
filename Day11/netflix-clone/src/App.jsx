bff39991bc79729ceb7e1d61e647b5aee4acd9e8
thekhushi09 <thekhushitomar10@gmail.com>
2024-12-08 12:20:39 +0530

    new

---
+++ Day11/netflix-clone/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MovieList from './MovieList'
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{backgroundColor: "black",color:"white",textAlign:"center"}}>Netflix Clone App</h1>
+      <MovieList/>
+      </div>
+  )
+}
+
+export default App
---
+++ Day11/netflix-clone/src/movies.js
@@ -0,0 +1,54 @@
+const movies=[
+    {
+        id: 1,
+        image: "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/5/p/7/2009-dvd-eagle-hindi-tarzan-the-wonder-car-original-imahfevqrdeqzths.jpeg?q=70",
+        title: 'TARZAN-THE WONDER CAR',
+        language: "hindi",
+        cost: 290,
+    },
+    {
+        id: 2,
+        image: "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/9/d/d/2013-blu-ray-eagle-hindi-golmaal-original-imagms36uz2hucpy.jpeg?q=70",
+        title: 'GOLMAAL',
+        language: "hindi",
+        cost: 380,
+
+    },
+
+    {
+        id: 3,
+        image: "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/4/s/t/2023-blu-ray-paramount-pictures-english-mission-impossible-dead-original-imaguw429xnwybtg.jpeg?q=70",
+        title: 'Mission: Impossible – Dead Reckoning Part',
+        language: "english",
+        cost: 490,
+
+    },
+
+    {
+        id: 4,
+        image: "https://rukminim2.flixcart.com/image/612/612/av-media/movies/q/w/c/gangs-of-wasseypur-i-original-imadnzhqzxzrgumh.jpeg?q=70",
+        title: 'Gangs Of Wasseypur I',
+        language: "hindi",
+        cost: 380,
+    },
+
+    {
+        id: 5,
+        image: "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/l/t/h/2022-blu-ray-20th-century-studios-english-avatar-2-original-imagvyqpzps5edgz.jpeg?q=70",
+        title: 'Avatar 2',
+        language: "english",
+        cost: 500,
+    },
+
+    {
+        id: 6,
+        image: "https://rukminim2.flixcart.com/image/612/612/kph8h3k0/movie/w/x/l/2021-3d-blu-ray-warner-bros-excel-home-videos-english-justice-original-imag3ph6wufmqamk.jpeg?q=70",
+        title: 'Justice League',
+        language: "english",
+        cost: 450,
+    },
+
+
+]
+
+export default movies;
